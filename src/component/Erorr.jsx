35fa7e22aed0f5985aba7d2f0684397635a45a20
Sagor <sagor.heaking@gmail.com>
2023-04-13 20:50:45 +0600

    error page added complete

--- src/component/Erorr.jsx
+++ src/component/Erorr.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { useRouteError } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 
 const Erorr = () => {
-    const error = useRouteError()
+    const { error, status } = useRouteError()
 
     return (
-        <div className='text-2xl m-auto text-neutral-500  '>
-        <h1>{error.status}</h1>
-        <h2>{error.data.sorry}</h2>
-        <p>
-          Go ahead and email {error.data.hrEmail} if you
-          feel like this is a mistake.
-        </p>
+        <div className='text-2xl text-neutral-500  container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+        <div className='max-w-md text-center'>
+          <h2 className='mb-8 font-extrabold text-9xl text-gray-600'>
+            <span className='sr-only'>Error</span> {status || 404}
+          </h2>
+          <p className='text-2xl font-semibold md:text-3xl mb-8'>
+            {error?.message}
+          </p>
+          <Link
+            to='/'
+            className='px-8 py-3 font-semibold rounded bg-cyan-200 text-gray-900'
+          >
+            Back to homepage
+          </Link>
+        </div>
       </div>
     );
 };
